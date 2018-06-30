6b2f0750594578b0b36117112e0c5dd9ea93e317
Thirdhuman <robert_t_orr@yahoo.com>
2018-06-30 05:00:38 -0400

    ..

--- Robert/map_app/app.R
+++ Robert/map_app/app.R
@@ -7,6 +7,10 @@
 #    http://shiny.rstudio.com/
 #
 
+rsconnect::setAccountInfo(name='thirdhuman',
+ 			  token='9ED0BCABBE9FC1AA7528BA7F6514FA3E',
+ 			  secret='OTsQxKA1hT7PVOJiYyp+qxVeeISFhqxEganPET0U')
+
 library(leaflet)
 library(shinydashboard)
 library(shiny)
@@ -17,38 +21,9 @@ library(dplyr)
 library(leaflet.minicharts)
 
 #setwd('/Users/rorr/PythonStuff/Project-ArmBop/Robert/map_app')
-origin=read.csv('www/origin_map.csv')
-origin$X=NULL
-str(origin)
-origin$type = 'origin'
 
-destination=read.csv('www/dest_map.csv')
-destination$X=NULL
-str(destination)
-destination$type = 'destination'
-
-map_data=rbind(destination, origin)
-str(map_data)
-
+map_data=read.csv('www/map_data.csv')
 map_data <- dplyr::filter(map_data, flights.sum > 0) %>% mutate_at(vars(lat, lon, flights.sum, cancelled.counter, delay.counter, nas_delay.counter,carrier_delay.counter,weather_delay.counter,security_delay.counter,late_aircraft_delay.counter),funs(as.numeric)) %>% na.omit
-
-map_data$cancelled.percent = map_data$cancelled.counter/map_data$flights.sum
-map_data$delay.percent = map_data$delay.counter/map_data$flights.sum
-map_data$nas_delay.percent = map_data$nas_delay.counter/map_data$flights.sum
-map_data$carrier_delay.percent = map_data$carrier_delay.counter/map_data$flights.sum
-map_data$weather_delay.percent = map_data$weather_delay.counter/map_data$flights.sum
-map_data$security_delay.percent = map_data$security_delay.counter/map_data$flights.sum
-map_data$delay.percent = map_data$delay.counter/map_data$flights.sum
-map_data$late_aircraft_delay.percent = map_data$late_aircraft_delay.counter/map_data$flights.sum
-
-map_data$not_cancelled.percent = 1- map_data$cancelled.counter/map_data$flights.sum
-map_data$not_delay.percent = 1- map_data$delay.counter/map_data$flights.sum
-map_data$not_nas_delay.percent = 1- map_data$nas_delay.counter/map_data$flights.sum
-map_data$not_carrier_delay.percent = 1- map_data$carrier_delay.counter/map_data$flights.sum
-map_data$not_weather_delay.percent = 1- map_data$weather_delay.counter/map_data$flights.sum
-map_data$not_security_delay.percent = 1- map_data$security_delay.counter/map_data$flights.sum
-map_data$not_delay.percent = 1- map_data$delay.counter/map_data$flights.sum
-map_data$not_late_aircraft_delay.percent = 1- map_data$late_aircraft_delay.counter/map_data$flights.sum
 
 
 # Define UI for application that draws a map
@@ -62,7 +37,7 @@ vars <- c(
   "Weather Delay" = "weather_delay.percent",
   "Security Delay" = "security_delay.percent",
   "Late Aircraft Delay" = "late_aircraft_delay.percent")
-
+shinyApp(
 ui <- dashboardPage(
   dashboardHeader(),
   dashboardSidebar(
@@ -78,7 +53,7 @@ ui <- dashboardPage(
   dashboardBody(
     tags$style(type = "text/css", "#MapPlot1 {height: calc(100vh - 80px) !important;}"),
     leafletOutput("MapPlot1")))
-  
+, 
 server = function(input, output) {
     output$MapPlot1 <- renderLeaflet({
      leaflet() %>% addProviderTiles("providers$Esri.NatGeoWorldMap") %>% 
@@ -109,10 +84,5 @@ server = function(input, output) {
 		    completedColor = "#7D4479")
    
   })
-}
-
-shinyApp(ui=ui,server=server,  options = list(height = 600))
-
-library(rsconnect)
-rsconnect::deployApp('/Users/rorr/PythonStuff/Project-ArmBop/Robert/map_app')
-
+})
+rsconnect::deployApp()
